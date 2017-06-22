f48312f9824ddfc280cc3e2dd9cef4990e35e4fc
fantasmaenlacascara <mwimpelberg@protonmail.com>
2017-06-22 12:21:05 +0000

    first commit

---
+++ files/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /tmp/deluge/deluge-1.3.11/; python setup.py build; python setup.py install; python setup.py install_data;
+
---
+++ setup.sh
@@ -0,0 +1,1 @@
+cd /tmp/deluge/deluge-1.3.11/;python setup.py build;sudo python setup.py install sudo python setup.py install_data;
